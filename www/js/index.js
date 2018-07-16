b8257122de72fabf697b5be4dd3046963d1095fd
symunona <symunona@gmail.com>
2018-07-15 20:15:02 -0500

    getting-there

--- www/js/index.js
+++ www/js/index.js
@@ -28,7 +28,7 @@ var app = {
             // Show welcome screen.
             app.game = JSON.parse(game);
             if (!app.game.status) {
-                app.showStartGame();
+                app.restoreGame();
             }
             else {
                 app.showPage('intro');
@@ -53,17 +53,69 @@ var app = {
         $('.start button.back').on('click', app.resetGame)
         $('button#start-game').on('click', app.startGame)
         $('.app').show();
+        $('.challange h1 button.back').on('click', app.resetGameWithConfirm)
+        $('.challange .ctrl-buttons button.back').on('click', app.prevChallange)
+        $('.challange .ctrl-buttons button.forward').on('click', app.nextChallange)
     },
 
-    startGame: function(){
-        $('.challange h1').html('Good enough');
-        $('.challange .content').html('here is some content');
+    startGame: function () {
+        app.showChallenge(0);
+        app.game.startTime = moment().format();
+        app.game.currentChallenge = 0;
+        console.warn('game started ', app.game.startTime)
+        app.saveGame();
+        app.registerNotifications();
+    },
+
+    showChallenge: function (index) {
+        app.game.currentChallenge = index;
+        var challenge = app.game.challenges[index]
+        $('.challange h1 .title').html(challenge.title);
+        $('.challange .content').html(challenge.description);
+        $('.challange .ctrl-buttons button').hide();
+
         app.showPage('challange');
+
+        // If there is a next challenge
+        if (index + 1 < app.game.challenges.length) {
+            if (this.isChallengeVisible(index + 1)) {
+                $('.ctrl-buttons button.forward').show();
+            }
+        }
+        if (index > 0) {
+            $('.ctrl-buttons button.back').show();
+        }
+    },
+
+    prevChallange: function () {
+        app.showChallenge(app.game.currentChallenge - 1);
+    },
+
+    nextChallange: function () {
+        app.showChallenge(app.game.currentChallenge + 1);
+    },
+
+    isChallengeVisible: function (index) {
+        if (moment().diff(moment(app.game.startTime), 'seconds') > app.game.challenges[index].time) {
+            return true;
+        }
+        return false;
     },
 
     resetGame: function () {
         app.game = false;
+        localStorage.removeItem(APP_STORAGE_KEY);
         app.showPage('intro');
+    },
+
+    resetGameWithConfirm: function () {
+        navigator.notification.confirm('Are you sure you want to stop the current game?', function () {
+            app.game.startTime = undefined;
+            app.game.currentChallenge = 0;
+            cordova.plugins.notification.local.clearAll();
+            app.saveGame();
+            app.showStartGame();
+        })
     },
 
     // deviceready Event Handler
@@ -88,13 +140,21 @@ var app = {
 
     onDownloadGame: function (key) {
         if (!key) { return; }
-        $.ajax({ url: key + '.json' }).then(function (gameData) {
+
+        // app.queryForPermissions();
+
+        $.ajax({ url: 'games/' + key + '.json', contentType: 'application/json' }).then(function (gameData) {
+            if (typeof (gameData) === 'string') gameData = JSON.parse(gameData);
             app.game = gameData;
-            localStorage.setItem('game-on', JSON.stringify(gameData));
+            app.saveGame();
             app.showStartGame();
         }).fail(function () {
             $('.error').html('Game loading error. Try another one.')
         })
+    },
+
+    saveGame: function () {
+        localStorage.setItem(APP_STORAGE_KEY, JSON.stringify(app.game));
     },
 
     canGameStartNow: function () {
@@ -116,12 +176,12 @@ var app = {
         return canStartNow;
     },
 
-    updateStartButton: function(){
-        if (app.canGameStartNow()){
+    updateStartButton: function () {
+        if (app.canGameStartNow()) {
             $('button#start-game').removeAttr('disabled');
-        } else{
+        } else {
             $('button#start-game').attr('disabled', true);
-        }        
+        }
     },
 
     showStartGame: function () {
@@ -130,16 +190,94 @@ var app = {
         app.updateStartButton();
         app.showPage('start')
     },
+    restoreGame: function () {
+        if (app.game.startTime) {
+            app.showChallenge(app.game.currentChallenge || 0);
+        }
+        else {
+            app.showStartGame();
+        }
+    },
 
     showPage: function (cls) {
         $('.app').children().hide();
-        $('.' + cls).show();
+        $('.app > .' + cls).show();
+        $('.app').scrollTop = 0;
     },
 
     loading: function (message) {
         $('.loading .message').html(message);
         app.showPage('loading');
+    },
 
+    error: function (message) {
+        $('.error .message').html(message);
+        app.showPage('error');
+    },
+
+    registerNotifications: function () {
+
+        if (!window.cordova) return;
+
+        cordova.plugins.notification.local.on('click', app.onNotificationClick)
+        cordova.plugins.notification.local.on('trigger', app.onNotificationTrigger);
+
+        var notifs = app.game.challenges.slice(1).map(function (c, i) {
+            return {
+                id: i + 1,
+                title: c.title,
+                text: c.description.substr(0, 40),
+                icon: 'res://e',
+                ongoing: true,
+                at: moment(app.game.startTime).add(c.time, 'seconds').toDate()
+            }
+        });
+        console.log('sceduling', notifs);
+        // console.log('times', notifs.map(function(n){return moment(n.at).diff(moment(), 'seconds')}))
+
+        cordova.plugins.notification.local.schedule(notifs);
+    },
+
+    onNotificationClick: function (notification) {
+        console.warn('onclick', arguments, notification, i);
+        app.showChallenge(notification.id);
+    },
+
+    onNotificationTrigger: function (notification) {
+        console.warn('SHOWING challenge', notification.id, notification)
+        app.showChallenge(notification.id);
+        if (notification.id > 0) {
+            cordova.plugins.notification.local.clear(notification.id - 1)
+        }
+    },
+
+    queryForPermissions: function () {
+
+        if (!window.cordova) return;
+
+        cordova.plugins.notification.local.hasPermission(function (granted) {
+
+            var triggerTime = moment().add(5, 'seconds').toDate();
+            console.log(triggerTime)
+            cordova.plugins.notification.local.schedule({
+                id: 1,
+                title: 'Sample notification',
+                text: '5 seconds in...',
+                icon: 'res://e',
+                at: triggerTime
+            });
+
+            var trigger2Time = moment().add(15, 'seconds').toDate();
+            console.log(trigger2Time)
+            cordova.plugins.notification.local.schedule({
+                id: 2,
+                title: 'and another one...',
+                text: '15 seconds in...',
+                icon: 'res://e',
+                at: trigger2Time
+            });
+
+        });
 
     }
 };
